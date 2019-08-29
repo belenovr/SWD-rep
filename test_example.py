d6e78a321661146105c7a2d78595a7dde25da7e3
Ruslan Belenov <r.belenov@quality-lab.ru>
2019-08-29 11:59:56 +0300

    Скрипт логина

--- test_example.py
+++ test_example.py
@@ -2,6 +2,9 @@ import time
 from selenium import webdriver
 
 driver = webdriver.Firefox()
-driver.get("http://www.google.com/")
+driver.get("http://localhost/litecart/admin/")
+driver.find_element_by_name("username").send_keys("admin")
+driver.find_element_by_name("password").send_keys("admin")
+driver.find_element_by_name("login").click()
 time.sleep(5)
 driver.quit()
