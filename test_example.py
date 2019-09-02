21b9e40ea6f5068aa9dd4ad983826e57d3a7463f
Ruslan Belenov <r.belenov@quality-lab.ru>
2019-09-02 17:56:36 +0300

    Задание №7

--- test_example.py
+++ test_example.py
@@ -6,5 +6,19 @@ driver.get("http://localhost/litecart/admin/")
 driver.find_element_by_name("username").send_keys("admin")
 driver.find_element_by_name("password").send_keys("admin")
 driver.find_element_by_name("login").click()
-time.sleep(5)
+
+time.sleep(1)
+vr = driver.find_elements_by_tag_name('li')
+i = 0
+while i < (57) :
+    vr[i].click()
+    i = i+1
+    time.sleep(1)
+    driver.find_elements_by_tag_name("h1")
+    vr = driver.find_elements_by_tag_name('li')
+    if len(vr) < i : i = 0
+
+
 driver.quit()
+
+
