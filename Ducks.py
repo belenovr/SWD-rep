b3e9ee2cc1fc8cb83e500d751c2a740ec023c16f
Ruslan Belenov <r.belenov@quality-lab.ru>
2019-09-03 19:44:06 +0300

    Задание 8

--- Ducks.py
+++ Ducks.py
@@ -3,14 +3,36 @@ from selenium import webdriver
 
 driver = webdriver.Firefox()
 driver.get("http://localhost/litecart/")
-duck = driver.find_elements_by_xpath('//div/div/ul/li/a/div/div')
-i = 1
-num = len(duck)+1
-duck[i].find_elements_by_class_name('sticker sale')
-#while i < num :
-#    duck[i].find_elements_by_class_name('sticker sale')
-#    i = 1 + 1
-#duck[1].click()
+#Первый блок
+duckBox = driver.find_element_by_id('box-most-popular')
+ducks = duckBox.find_elements_by_xpath("./div/ul/li/a[@class='link']")
+i = len(ducks)
+while i > 0 :
+    try :
+        ducks[i-1].find_element_by_xpath("./div/div[@title='New']")
+    except BaseException :
+        ducks[i-1].find_element_by_xpath("./div/div[contains(text(),'Sale')]")
+    i = i - 1
+#Второй блок
+duckBox = driver.find_element_by_id('box-campaigns')
+ducks = duckBox.find_elements_by_xpath("./div/ul/li/a[@class='link']")
+i = len(ducks)
+while i > 0 :
+    try :
+        ducks[i-1].find_element_by_xpath("./div/div[@title='New']")
+    except BaseException :
+        ducks[i-1].find_element_by_xpath("./div/div[contains(text(),'Sale')]")
+    i = i - 1
+#Tретий блок
+duckBox = driver.find_element_by_id('box-latest-products')
+ducks = duckBox.find_elements_by_xpath("./div/ul/li/a[@class='link']")
+i = len(ducks)
+while i > 0 :
+    try :
+        ducks[i-1].find_element_by_xpath("./div/div[@title='New']")
+    except BaseException :
+        ducks[i-1].find_element_by_xpath("./div/div[contains(text(),'Sale')]")
+    i = i - 1
 
 time.sleep(1)
 
