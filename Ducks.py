78784dc383634f18e39dd182c480ce43edc3378e
Ruslan Belenov <r.belenov@quality-lab.ru>
2019-09-11 10:50:10 +0300

    Задание 8

--- Ducks.py
+++ Ducks.py
@@ -8,32 +8,30 @@ duckBox = driver.find_element_by_id('box-most-popular')
 ducks = duckBox.find_elements_by_xpath("./div/ul/li/a[@class='link']")
 i = len(ducks)
 while i > 0 :
-    try :
-        ducks[i-1].find_element_by_xpath("./div/div[@title='New']")
-    except BaseException :
-        ducks[i-1].find_element_by_xpath("./div/div[contains(text(),'Sale')]")
-    i = i - 1
+    st = len(ducks[i-1].find_elements_by_xpath("./div/div"))
+    if st == 1:
+        i = i - 1
+    else:
+        break
 #Второй блок
 duckBox = driver.find_element_by_id('box-campaigns')
 ducks = duckBox.find_elements_by_xpath("./div/ul/li/a[@class='link']")
 i = len(ducks)
 while i > 0 :
-    try :
-        ducks[i-1].find_element_by_xpath("./div/div[@title='New']")
-    except BaseException :
-        ducks[i-1].find_element_by_xpath("./div/div[contains(text(),'Sale')]")
-    i = i - 1
+    st = len(ducks[i-1].find_elements_by_xpath("./div/div"))
+    if st == 1:
+        i = i - 1
+    else:
+        break
 #Tретий блок
 duckBox = driver.find_element_by_id('box-latest-products')
 ducks = duckBox.find_elements_by_xpath("./div/ul/li/a[@class='link']")
 i = len(ducks)
 while i > 0 :
-    try :
-        ducks[i-1].find_element_by_xpath("./div/div[@title='New']")
-    except BaseException :
-        ducks[i-1].find_element_by_xpath("./div/div[contains(text(),'Sale')]")
-    i = i - 1
-
+    st = len(ducks[i-1].find_elements_by_xpath("./div/div"))
+    if st == 1:
+        i = i - 1
+    else:
+        break
 time.sleep(1)
-
 driver.quit()
