a0dd4b159b8f19949ab68dd81af846513940094f
Ruslan Belenov <r.belenov@quality-lab.ru>
2019-09-11 12:31:46 +0300

    Задание 7

--- test_example.py
+++ test_example.py
@@ -1,40 +1,35 @@
 import time
 from selenium import webdriver
 
-driver = webdriver.Firefox()
+#driver = webdriver.Firefox()
+driver = webdriver.Chrome("/home/pirate/tools/chromedriver")
 driver.get("http://localhost/litecart/admin/")
 driver.find_element_by_name("username").send_keys("admin")
 driver.find_element_by_name("password").send_keys("admin")
 driver.find_element_by_name("login").click()
 block = driver.find_element_by_id('box-apps-menu-wrapper') #Определение блока меню
 list = block.find_elements_by_xpath("./ul/li[@id='app-']") #Определение пунктов меню
-i = len(list)
-k = 1
-while i > 0 :
+for i in range(0, len(list)-1) :
+    block = driver.find_element_by_id('box-apps-menu-wrapper')  # Определение блока меню
+    list = block.find_elements_by_xpath("./ul/li[@id='app-']")  # Определение пунктов меню
+    list[i].click()
+    time.sleep(1)
+    driver.find_elements_by_tag_name("h1")
     try:
-        block = driver.find_element_by_id('box-apps-menu-wrapper')  # Определение блока меню
-        list = block.find_elements_by_xpath("./ul/li[@id='app-']")  # Определение пунктов меню
-        f = list[i-1]
-        sublist = f.find_elements_by_xpath('./ul/li/a') #Определение подпунктов меню
-        list[i - 1].click()
-        i = i - 1
+        sublist = list[i].find_elements_by_xpath('./ul/li') #Определение подпунктов меню
         k = len(sublist)
-        while k > 0:
+    except BaseException:
+        k = 0
+    if k > 0:
+        for j in range(0,k-1):
             block = driver.find_element_by_id('box-apps-menu-wrapper')  # Определение блока меню
             list = block.find_elements_by_xpath("./ul/li[@id='app-']")  # Определение пунктов меню
-            sublist = list[i - 1].find_elements_by_xpath("./li/a")  # Определение подпунктов меню
-            sublist[k-1].click()
+            sublist = list[i].find_elements_by_xpath('./ul/li/a') #Определение подпунктов меню
+            sublist[j].click()
             time.sleep(1)
             driver.find_elements_by_tag_name("h1")
-            k = k - 1
-    except BaseException:
-        i = i - 1
-
 
 time.sleep(1)
-
-
-
 driver.quit()
 
 
