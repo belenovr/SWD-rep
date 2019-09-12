32a5ad406d4d5c3e25d1fe69e6c736c89c822267
Ruslan Belenov <r.belenov@quality-lab.ru>
2019-09-12 21:43:18 +0300

    Задание 8

--- Ducks.py
+++ Ducks.py
@@ -3,35 +3,14 @@ from selenium import webdriver
 
 driver = webdriver.Firefox()
 driver.get("http://localhost/litecart/")
-#Первый блок
-duckBox = driver.find_element_by_id('box-most-popular')
-ducks = duckBox.find_elements_by_xpath("./div/ul/li/a[@class='link']")
+ducks = driver.find_elements_by_css_selector("[class ^=product]")
 i = len(ducks)
-while i > 0 :
-    st = len(ducks[i-1].find_elements_by_xpath("./div/div"))
+for j in range(0,i-1):
+    st = len(ducks[j].find_elements_by_css_selector("[class ^=sticker]"))
     if st == 1:
         i = i - 1
     else:
-        break
-#Второй блок
-duckBox = driver.find_element_by_id('box-campaigns')
-ducks = duckBox.find_elements_by_xpath("./div/ul/li/a[@class='link']")
-i = len(ducks)
-while i > 0 :
-    st = len(ducks[i-1].find_elements_by_xpath("./div/div"))
-    if st == 1:
-        i = i - 1
-    else:
-        break
-#Tретий блок
-duckBox = driver.find_element_by_id('box-latest-products')
-ducks = duckBox.find_elements_by_xpath("./div/ul/li/a[@class='link']")
-i = len(ducks)
-while i > 0 :
-    st = len(ducks[i-1].find_elements_by_xpath("./div/div"))
-    if st == 1:
-        i = i - 1
-    else:
-        break
+        passed = False
+        assert passed, 'Not passed'
 time.sleep(1)
 driver.quit()
